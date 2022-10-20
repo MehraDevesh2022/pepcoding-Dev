532bffb499daee59976c741d14e1f7a2fb109f3e
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-10-20 10:14:09 +0530

    then and catch with promises

--- Ip/promises/01-promise.js
+++ Ip/promises/01-promise.js
@@ -4,20 +4,41 @@
 // 2: fullfilment and then sattled
 // 3: reject and cacth sattled
 
-
-//*************************** how call back handle a async operation **************************:
-
 const fs = require("fs");
-fs.readFile("./f1.txt" ,cb);
-function cb(error ,data) {
-    if(error){
-        console.log(error);
-    }else{
-        console.log(data);
-    }
+//*************************** how call-back handle a async operation **************************:
 
-}
+
+// fs.readFile("./f1.txt" ,cb);
+// function cb(error ,data) {
+//     if(error){
+//         console.log(error);
+//     }else{
+//         console.log(data);
+//     }
+
+// }
 //****************************************************************************************** */
 
 // How to promise work : with async js
+//-----------------------------------------------\\
+// let promiseVar = fs.promises.readFile('./f1.txt')
+// console.log(promiseVar); // Promise { <pending> }
+//--------------------------------------------------\\
+// promise handle with settimeout() || then and catch method : here we are working with then and  catch
+// then => it handle fullfilment of promise  
+// catch => if promise rejcetd then error handle by catch method
+
+
+//------------------------------------------------------\\
+
+console.log("Before");
+const promiseVar = fs.promises.readFile('./f1.txt' , 'utf-8');
+promiseVar.then((data) =>{ // fulfilment handle and sattle it
+        console.log(data); 
+})
+promiseVar.catch((err)=>{ // rejextion handle here
+    console.log(err);
+})
+console.log("after");
+
 
