6758cc642e007faf5ea5a450a22fd7e5fee818cf
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-10-21 22:03:06 +0530

    promise with then chaining

--- Ip/promises/04-readFileSerially.js
+++ Ip/promises/04-readFileSerially.js
@@ -2,22 +2,22 @@ const fs = require("fs");
 
 // promise serially
 
-let f1 = fs.promises.readFile("./f1.txt", "utf-8");
-let f2 = fs.promises.readFile("./f2.txt", "utf-8");
-let f3 = fs.promises.readFile("./f3.txt", "utf-8");
+// let f1 = fs.promises.readFile("./f1.txt", "utf-8");
+// let f2 = fs.promises.readFile("./f2.txt", "utf-8");
+// let f3 = fs.promises.readFile("./f3.txt", "utf-8");
 
-f1.then(function cb(data){
-    console.log(data);
-    f2.then(cb2)
-})
-function cb2(data) {
-  console.log(data);
-  f3.then(cb3);
-}
+// f1.then(function cb(data){
+//     console.log(data);
+//     f2.then(cb2)
+// })
+// function cb2(data) {
+//   console.log(data);
+//   f3.then(cb3);
+// }
 
-function cb3(data) {
-  console.log(data);
-}
+// function cb3(data) {
+//   console.log(data);
+// }
 
 
 // order maintaind always
@@ -29,3 +29,23 @@ f2 txt file
 f3 txt file 3
 
 */
+
+// imrovment of code using then() method chaning
+
+let f1 = fs.promises.readFile("./f1.txt", "utf-8");
+let f2 = fs.promises.readFile("./f2.txt", "utf-8");
+let f3 = fs.promises.readFile("./f3.txt", "utf-8");
+
+f1.then(function cb(data) {
+  console.log(data);
+  return f2
+}).then(cb2).then(cb3).catch((err) => console.log(err));
+
+function cb2(data) {
+  console.log(data);
+  return f3
+}
+
+function cb3(data) {
+  console.log(data);
+}
