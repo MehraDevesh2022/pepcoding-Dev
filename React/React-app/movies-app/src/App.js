2566aac63e53aedfe18707b477228fbbc70faf98
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-11-07 23:37:21 +0530

    revision

--- React/React-app/movies-app/src/App.js
+++ React/React-app/movies-app/src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
       <NavBar/>
-      <Banner/>
-     <MoviesList/>
+      {/* <Banner/> */}
+     {/* <MoviesList/> */}
     </>
   );
 }
--- React/React-app/movies-app/src/component/NavBar.js
+++ React/React-app/movies-app/src/component/NavBar.js
@@ -6,7 +6,7 @@ export class NavBar extends Component {
        render(){
    return (
      <div style={{display : 'flex' , padding : '0.5rem'}}>
-       <h1>Movies App</h1>
+       <h1>MoviesApp</h1>
        <h2 style={{marginLeft : '2rem' , marginTop : '0.3rem'}}>Favourites</h2>
      </div>
    );
