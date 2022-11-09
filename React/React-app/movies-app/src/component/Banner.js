75d829853af4e71963e6d64e904d5ed51af391a5
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-11-09 22:21:40 +0530

    my txt file added

--- React/React-app/movies-app/src/component/Banner.js
+++ React/React-app/movies-app/src/component/Banner.js
@@ -2,21 +2,16 @@ import React, { Component } from "react";
 import {movies} from '../movieData'
 export class Banner extends Component {
   render() {
+    let randomNum = Math.floor(Math.random() * movies.results.length)
     return (
-      <div className="card banner-card">
-        <img
-          src={`https://image.tmdb.org/t/p/original${movies.results[Math.floor(Math.random() * movies.results.length) +1 ].backdrop_path}`}
-          className="card-img-top banner-img"
-          alt="..."
-        />
-
-        <h5 className="card-title banner-title">Card title</h5>
-        <p className="card-text banner-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-      </div>
-    );
+        <div className="card banner-card">
+        <img src={`https://image.tmdb.org/t/p/original${movies.results[randomNum].poster_path}` } className="card-img-top banner-img" alt="..."/>
+        
+            <h5 className="card-title banner-title">{movies.results[randomNum].title}</h5>
+            {/* <p className="card-text banner-text">{movies.results[randomNum].overview}</p> */}
+          </div>
+      
+    )
   }
 }
 export default Banner
--- React/React-app/movies-app/src/component/NavBar.js
+++ React/React-app/movies-app/src/component/NavBar.js
@@ -1,17 +1,16 @@
-import  React, { Component } from "react";
+import React, { Component } from "react";
 
 export class NavBar extends Component {
-      
 
-       render(){
-   return (
-     <div style={{display : 'flex' , padding : '0.5rem'}}>
-       <h1>MoviesApp</h1>
-       <h2 style={{marginLeft : '2rem' , marginTop : '0.3rem'}}>Favourites</h2>
-     </div>
-   );
-       }
+
+  render() {
+    return (
+      <div style={{ display: 'flex', padding: '0.5rem' }}>
+        <h1>MoviesApp</h1>
+        <h2 style={{ marginLeft: '2rem', marginTop: '0.3rem' }}>Favourites</h2>
+      </div>
+    );
+  }
 }
 
 export default NavBar
-
